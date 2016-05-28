03eda2b076c927ad55167c0515dd85f0e3ad4558
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-28 15:43:21 +1200

    Initial Commit

---
+++ Wellington Bus Stops/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Wellington Bus Stops
+//
+//  Created by Thomas Horrobin on 28/05/16.
+//  Copyright © 2016 Wellington City Council API Extensions. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
