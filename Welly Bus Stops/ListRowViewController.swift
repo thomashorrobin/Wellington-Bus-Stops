ae2f4adb23484cec418c3fd20fabb45079f76e5a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-28 16:27:57 +1200

    Added today extension scaffolded code

---
+++ Welly Bus Stops/ListRowViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ListRowViewController.swift
+//  Welly Bus Stops
+//
+//  Created by Thomas Horrobin on 28/05/16.
+//  Copyright © 2016 Wellington City Council API Extensions. All rights reserved.
+//
+
+import Cocoa
+
+class ListRowViewController: NSViewController {
+
+    override var nibName: String? {
+        return "ListRowViewController"
+    }
+
+    override func loadView() {
+        super.loadView()
+
+        // Insert code here to customize the view
+    }
+
+}
