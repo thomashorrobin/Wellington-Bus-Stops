afea8f49725f81a06a38bfbe4f5e2bf151f602cd
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-28 19:06:04 +1200

    added static api calls to the bus stops class

--- Wellington Bus Stops/BusStop.swift
+++ Wellington Bus Stops/BusStop.swift
@@ -41,4 +41,63 @@ class BusStop: NSObject, MKAnnotation {
         self.longitude = Double(stop["Long"] as! String)!
         
     }
+    
+    class func getStop(sms: String, completion: (busStop: BusStop) -> ()) {
+        let getEndpoint: String = "https://www.metlink.org.nz/api/v1/Stop/" + sms.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())!
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: getEndpoint)!
+        let task = session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            do {
+                
+                // Parse the JSON to get the IP
+                let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                let bs = BusStop(stop: jsonDictionary)
+                completion(busStop: bs)
+            } catch {
+                print("bad things happened")
+            }
+        })
+        
+        task.resume()
+    }
+    
+    class func getStopsGeo(completion: (busStop: BusStop) -> ()) {
+        let getEndpoint: String = "https://www.metlink.org.nz/api/v1/StopNearby/-41.28916701/174.77593559000002"
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: getEndpoint)!
+        let task = session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            do {
+                
+                // Parse the JSON to get the IP
+                let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSArray
+                
+                for j in jsonDictionary {
+                    let bs = BusStop(stop: j as! NSDictionary)
+                    completion(busStop: bs)
+                }
+            } catch {
+                print("bad things happened")
+            }
+        })
+        
+        task.resume()
+    }
 }
