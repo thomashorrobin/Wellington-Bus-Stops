6d90e83af903e576e28e59372e0e3cfeb41de388
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-05-31 00:20:31 +1200

    added bus stop class for widget target

---
+++ Welly Bus Stops/BusStop.swift
@@ -0,0 +1,56 @@
+//
+//  BusStop.swift
+//  Wellington Bus Stops
+//
+//  Created by Thomas Horrobin on 30/05/16.
+//  Copyright © 2016 Wellington City Council API Extensions. All rights reserved.
+//
+
+import Foundation
+
+class BusStop2: NSObject {
+    var name: String
+    var sms: String
+    
+    override var description: String{
+        return name
+    }
+    
+    init(busStopName: String, sms: String) {
+        self.name = busStopName
+        self.sms = sms
+    }
+    
+    init(stop: NSDictionary) {
+        self.name = stop["Name"] as! String!
+        self.sms = stop["Sms"] as! String!
+    }
+    
+    class func getStop(sms: String, completion: (busStop: BusStop2) -> ()) {
+        let getEndpoint: String = "https://www.metlink.org.nz/api/v1/Stop/" + sms.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet())!
+        let session = NSURLSession.sharedSession()
+        let url = NSURL(string: getEndpoint)!
+        let task = session.dataTaskWithURL(url, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            
+            // Read the JSON
+            do {
+                
+                // Parse the JSON to get the IP
+                let jsonDictionary = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                let bs = BusStop2(stop: jsonDictionary)
+                completion(busStop: bs)
+            } catch {
+                print("bad things happened")
+            }
+        })
+        
+        task.resume()
+    }
+}
