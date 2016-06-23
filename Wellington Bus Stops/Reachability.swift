1ae338cd1db50f8a67f5289f1426e671fb9e2f3a
Thomas Horrobin <thomasroberthorrobin@gmail.com>
2016-06-23 15:57:52 +1200

    Reachability class created

---
+++ Wellington Bus Stops/Reachability.swift
@@ -0,0 +1,37 @@
+//
+//  Reachability.swift
+//  Wellington Bus Stops
+//
+//  Created by Thomas Horrobin on 23/06/16.
+//  Copyright © 2016 Wellington City Council API Extensions. All rights reserved.
+//
+
+//import Foundation
+import Cocoa
+import SystemConfiguration
+
+public class Reachability {
+    class func isConnectedToNetwork(alertUser: Bool) -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        let connected = isReachable && !needsConnection
+        if alertUser && !connected {
+            let alert = NSAlert()
+            alert.messageText = "You're not connected to the internet."
+            alert.informativeText = "Wellington Bus Stops requires a internet connection to work properly. Please connect to the internet and try again :)"
+            alert.addButtonWithTitle("OK")
+            alert.runModal()
+        }
+        return connected
+    }
+}
